5b12ccc57677621fadd417a5582c439ffa5a6d8d
EsquivelSebastian <esquivel.sebastian@me.com>
2022-10-09 11:12:23 -0300

    Header & About

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 let menu = document.getElementById('menu');
+let menuColor = document.getElementById('menu');
 let menuBars = document.getElementById('menuBars');
+let menuHold = document.querySelector("#nav");
+
 
 menuBars.addEventListener('click', toggleMenu);
-
 function toggleMenu(){
     menu.classList.toggle('show-menu');
 }
+
+window.addEventListener('scroll', function(){
+    menuHold.classList.toggle('active', this.window.scrollY > 0)
+})
+
+let punto = window.addEventListener('scroll',function(){
+    this.window.screenY
+})
